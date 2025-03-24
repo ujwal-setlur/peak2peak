d7ef3a43aaf2594654cdaa246ad7451d384d3ad8
Athul Rajeev <athul@evolvier.com>
2025-03-24 11:56:25 +0000

    responsive ui fixes

--- web/src/components/PostCard.jsx
+++ web/src/components/PostCard.jsx
@@ -17,18 +17,24 @@ const PostCard = ({ data, onClick }) => {
       />
       <div class="absolute inset-0 flex items-end justify-center gap-[60px] px-4 py-3">
         {data.title && (
-          <span class="line-clamp-2 text-[1.65rem] font-medium text-white">{data.title}</span>
+          <span class="line-clamp-2 text-sm font-medium text-white sm:text-base lg:text-[1.65rem]">
+            {data.title}
+          </span>
         )}
       </div>
       <div class="absolute inset-0 flex bg-black opacity-0 duration-300 group-hover:cursor-pointer group-hover:opacity-30 group-hover:transition-opacity"></div>
-      <div class="absolute inset-0 flex items-center justify-center gap-[60px] opacity-0 duration-300 group-hover:cursor-pointer group-hover:opacity-100 group-hover:transition-opacity">
+      <div class="absolute inset-0 flex items-center justify-center gap-[30px] opacity-0 duration-300 group-hover:cursor-pointer group-hover:opacity-100 group-hover:transition-opacity sm:gap-[45px] lg:gap-[60px]">
         <div class="flex flex-col items-center justify-center gap-2">
-          <img src={LikeIcon.src} alt="like" class="h-10" />
-          <span class="text-md font-medium text-white">{data.likesCount || 0}</span>
+          <img src={LikeIcon.src} alt="like" class="h-6 sm:h-7 lg:h-10" />
+          <span class="text-sm font-medium text-white sm:text-base lg:text-md">
+            {data.likesCount || 0}
+          </span>
         </div>
         <div class="flex flex-col items-center justify-center gap-2">
-          <img src={CommentIcon.src} alt="like" class="h-10" />
-          <span class="text-md font-medium text-white">{data.commentsCount || 0}</span>
+          <img src={CommentIcon.src} alt="like" class="h-6 sm:h-7 lg:h-10" />
+          <span class="text-sm font-medium text-white sm:text-base lg:text-md">
+            {data.commentsCount || 0}
+          </span>
         </div>
       </div>
     </div>
