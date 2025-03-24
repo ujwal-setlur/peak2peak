d0a90df670065254dc333070b2ab3039105e3335
Athul Rajeev <athul@evolvier.com>
2025-03-24 07:07:00 +0000

    tag line added, post title fix

--- web/src/components/PostCard.jsx
+++ web/src/components/PostCard.jsx
@@ -15,6 +15,11 @@ const PostCard = ({ data, onClick }) => {
         alt="post"
         class="cover aspect-square w-full overflow-hidden duration-300 ease-in group-hover:scale-110 group-hover:transform"
       />
+      <div class="absolute inset-0 flex items-end justify-center gap-[60px] px-4 py-3">
+        {data.title && (
+          <span class="line-clamp-2 text-[1.65rem] font-medium text-white">{data.title}</span>
+        )}
+      </div>
       <div class="absolute inset-0 flex bg-black opacity-0 duration-300 group-hover:cursor-pointer group-hover:opacity-30 group-hover:transition-opacity"></div>
       <div class="absolute inset-0 flex items-center justify-center gap-[60px] opacity-0 duration-300 group-hover:cursor-pointer group-hover:opacity-100 group-hover:transition-opacity">
         <div class="flex flex-col items-center justify-center gap-2">
@@ -25,11 +30,6 @@ const PostCard = ({ data, onClick }) => {
           <img src={CommentIcon.src} alt="like" class="h-10" />
           <span class="text-md font-medium text-white">{data.commentsCount || 0}</span>
         </div>
-      </div>
-      <div class="absolute inset-0 flex items-end justify-center gap-[60px] px-4 py-3 opacity-0 duration-300 group-hover:cursor-pointer group-hover:opacity-100 group-hover:transition-opacity">
-        {data.title && (
-          <span class="line-clamp-3 text-base font-medium text-white">{data.title}</span>
-        )}
       </div>
     </div>
   );
