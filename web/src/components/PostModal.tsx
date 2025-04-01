d1f1efade8274ae69411f1b2513ce3ab2c87e5a7
Athul Rajeev <athul@evolvier.com>
2025-04-01 11:39:51 +0000

    add comment api integration

--- web/src/components/PostModal.tsx
+++ web/src/components/PostModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { fetchPostDetails, toggleLike } from '../lib/graphql';
+import { addComment, fetchPostDetails, toggleLike } from '../lib/graphql';
 
 import LikeIcon from '../assets/like-primary.svg';
 import LikedIcon from '../assets/liked.svg';
@@ -104,6 +104,194 @@ export const PostModal: React.FC<PostModalProps> = ({ isOpen, postId, onClose })
     }
   };
 
+  // Function to handle submit add comment form
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const comment = formData.get('comment') as string;
+    const name = formData.get('name') as string;
+    const email = formData.get('email') as string;
+
+    if (!comment || !name || !email) {
+      alert('All fields are required!');
+      return;
+    }
+
+    try {
+      await addComment(postId, email, comment, name);
+      fetchPostDetailsData(postId as string, true);
+      form.reset();
+    } catch (error) {
+      console.log('error', error);
+      console.error('Error adding comment:', error);
+      alert('Failed to add comment.');
+    }
+  };
+
+  const renderCloseButton = () => {
+    return (
+      <button
+        className="group absolute left-0 top-0 flex h-16 w-16 items-center justify-center overflow-hidden bg-primary text-gray-400 hover:text-gray-900 dark:hover:text-white"
+        data-modal-close
+        data-post-id={postId}
+        onClick={onClose}
+      >
+        <svg className="h-15 w-15" fill="#ffffff" stroke="#ffffff" viewBox="0 0 24 24">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1"
+            d="M6 18L18 6M6 6l12 12"
+          ></path>
+        </svg>
+      </button>
+    );
+  };
+
+  const renderImageSection = () => {
+    return (
+      <img
+        src={postDetails?.images?.[0]?.url || ''}
+        alt="post"
+        className="aspect-square w-full overflow-hidden object-cover"
+      />
+    );
+  };
+
+  const renderTitle = () => {
+    return (
+      <div className="w-full text-lg font-semibold leading-tight text-primary">
+        {postDetails?.title || ''}
+      </div>
+    );
+  };
+
+  const renderDateAndCategorySection = () => {
+    return (
+      <div className="w-full text-base font-thin text-black">
+        {postDetails?.createdAt ? formatDate(postDetails?.createdAt) : ''}
+        <span className="mx-2 font-thin">|</span>
+        {postDetails?.blog?.Name || ''}
+      </div>
+    );
+  };
+
+  const renderLikeAndCommentSection = () => {
+    return (
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-4">
+          <button
+            className="text-teal-600 hover:text-teal-800"
+            data-action="like"
+            data-post-id={postId}
+            disabled={isLikeLoading}
+            onClick={handleClickLike}
+          >
+            <img
+              src={postDetails?.isLiked ? LikedIcon.src : LikeIcon.src}
+              alt="like"
+              className="h-8 w-8 fill-primary"
+            />
+          </button>
+          <span className="text-md font-medium text-black" id={`likes-${postId}`}>
+            {postDetails?.likeCounts || 0}
+          </span>
+        </div>
+        <div className="text-md font-thin text-black">|</div>
+        <div className="flex items-center gap-4">
+          <button
+            className="cursor-default text-teal-600 hover:text-teal-800"
+            data-action="like"
+            data-post-id={postId}
+          >
+            <img src={CommentIcon.src} alt="comment" className="h-8 w-8 fill-primary" />
+          </button>
+          <span className="text-md font-medium text-black" id={`comments-count-${postId}`}>
+            {postDetails?.commentCount || 0}
+          </span>
+        </div>
+      </div>
+    );
+  };
+
+  const renderDescription = () => {
+    return (
+      <div
+        className="w-full text-sm font-thin text-black"
+        dangerouslySetInnerHTML={{
+          __html: postDetails?.description ? strapiRichTextToHtml(postDetails?.description) : '',
+        }}
+      />
+    );
+  };
+
+  const renderComments = () => {
+    return (
+      <ul className="mt-8 max-h-[500px] space-y-3 overflow-y-auto" id={`comments-${postId}`}>
+        {postDetails && postDetails?.comments && postDetails?.comments?.length
+          ? postDetails.comments.map((comment, index) => {
+              const isLastComment =
+                postDetails?.comments?.length && index === postDetails.comments.length - 1;
+              return (
+                <li
+                  key={index}
+                  className={`flex gap-3 ${!isLastComment ? 'border-b-[1px] pb-3' : ''}`}
+                >
+                  <div className="flex h-[50px] w-[50px] shrink-0 items-center justify-center rounded-full bg-primary text-sm font-medium text-white">
+                    {comment.user ? getInitials(comment.user) : ''}
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-xs font-medium text-black">{comment.user}</span>
+                    <span className="text-xs font-thin text-black">{comment.comment}</span>
+                  </div>
+                </li>
+              );
+            })
+          : null}
+      </ul>
+    );
+  };
+
+  const renderAddCommentForm = () => {
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-3"
+        data-comment-form
+        data-post-id={postId}
+      >
+        <div className="mt-3 flex flex-col gap-2">
+          <textarea
+            name="comment"
+            placeholder="Write your comments..."
+            className="w-full border px-3 py-2 text-xs italic focus:outline-none focus:ring-2 focus:ring-primary"
+          ></textarea>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            className="w-full border px-3 py-2 text-xs italic focus:outline-none focus:ring-2 focus:ring-primary"
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="w-full border px-3 py-2 text-xs italic focus:outline-none focus:ring-2 focus:ring-primary"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-[150px] self-start bg-gradient-to-r from-teal-500 to-teal-800 px-1 py-2 text-xs tracking-wider text-white hover:from-teal-800 hover:to-teal-500 hover:font-medium"
+        >
+          SUBMIT
+        </button>
+      </form>
+    );
+  };
+
   if (!isOpen || loading) {
     return null;
   }
@@ -117,143 +305,20 @@ export const PostModal: React.FC<PostModalProps> = ({ isOpen, postId, onClose })
       <div className="overscroll-y-hidden relative z-10 h-full max-h-[80vh] w-full max-w-screen-2xl overflow-x-hidden border-[1px] border-primary bg-white p-3 shadow-lg md:p-5 md:pr-0">
         <div className="flex h-full w-full gap-2">
           <div className="relative flex aspect-square w-full max-w-[60%]">
-            <img
-              src={postDetails?.images?.[0]?.url || ''}
-              alt="post"
-              className="aspect-square w-full overflow-hidden object-cover"
-            />
-
-            <button
-              className="group absolute left-0 top-0 flex h-16 w-16 items-center justify-center overflow-hidden bg-primary text-gray-400 hover:text-gray-900 dark:hover:text-white"
-              data-modal-close
-              data-post-id={postId}
-              onClick={onClose}
-            >
-              <svg className="h-15 w-15" fill="#ffffff" stroke="#ffffff" viewBox="0 0 24 24">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            </button>
+            {renderImageSection()}
+            {renderCloseButton()}
           </div>
 
           <div className="w-full max-w-[40%] space-y-4 overflow-y-auto">
             <div className="flex w-full flex-col gap-3 bg-white bg-opacity-5 px-5 pb-5">
-              <div className="w-full text-lg font-semibold leading-tight text-primary">
-                {postDetails?.title || ''}
-              </div>
-
-              <div className="w-full text-base font-thin text-black">
-                {postDetails?.createdAt ? formatDate(postDetails?.createdAt) : ''}
-                <span className="mx-2 font-thin">|</span>
-                {postDetails?.blog?.Name || ''}
-              </div>
-
-              <div className="flex items-center gap-5">
-                <div className="flex items-center gap-4">
-                  <button
-                    className="text-teal-600 hover:text-teal-800"
-                    data-action="like"
-                    data-post-id={postId}
-                    disabled={isLikeLoading}
-                    onClick={handleClickLike}
-                  >
-                    <img
-                      src={postDetails?.isLiked ? LikedIcon.src : LikeIcon.src}
-                      alt="like"
-                      className="h-8 w-8 fill-primary"
-                    />
-                  </button>
-                  <span className="text-md font-medium text-black" id={`likes-${postId}`}>
-                    {postDetails?.likeCounts || 0}
-                  </span>
-                </div>
-                <div className="text-md font-thin text-black">|</div>
-                <div className="flex items-center gap-4">
-                  <button
-                    className="cursor-default text-teal-600 hover:text-teal-800"
-                    data-action="like"
-                    data-post-id={postId}
-                  >
-                    <img src={CommentIcon.src} alt="comment" className="h-8 w-8 fill-primary" />
-                  </button>
-                  <span className="text-md font-medium text-black" id={`comments-count-${postId}`}>
-                    {postDetails?.commentCount || 0}
-                  </span>
-                </div>
-              </div>
-
-              <div
-                className="w-full text-sm font-thin text-black"
-                dangerouslySetInnerHTML={{
-                  __html: postDetails?.description
-                    ? strapiRichTextToHtml(postDetails?.description)
-                    : '',
-                }}
-              />
-
+              {renderTitle()}
+              {renderDateAndCategorySection()}
+              {renderLikeAndCommentSection()}
+              {renderDescription()}
               <div className="mt-5 w-full">
                 <h3 className="text-sm font-semibold text-black">Comments</h3>
-
-                <form className="flex flex-col gap-3" data-comment-form data-post-id={postId}>
-                  <div className="mt-3 flex flex-col gap-2">
-                    <textarea
-                      name="comment"
-                      placeholder="Write your comments..."
-                      className="w-full border px-3 py-2 text-xs italic focus:outline-none focus:ring-2 focus:ring-primary"
-                    ></textarea>
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Your name"
-                      className="w-full border px-3 py-2 text-xs italic focus:outline-none focus:ring-2 focus:ring-primary"
-                    />
-                    <input
-                      type="text"
-                      name="email"
-                      placeholder="Email"
-                      className="w-full border px-3 py-2 text-xs italic focus:outline-none focus:ring-2 focus:ring-primary"
-                    />
-                  </div>
-                  <button
-                    type="button"
-                    className="w-[150px] self-start bg-gradient-to-r from-teal-500 to-teal-800 px-1 py-2 text-xs tracking-wider text-white hover:from-teal-800 hover:to-teal-500 hover:font-medium"
-                  >
-                    SUBMIT
-                  </button>
-                </form>
-
-                <ul
-                  className="mt-8 max-h-[500px] space-y-3 overflow-y-auto"
-                  id={`comments-${postId}`}
-                >
-                  {postDetails && postDetails?.comments && postDetails?.comments?.length
-                    ? postDetails.comments.map((comment, index) => {
-                        const isLastComment =
-                          postDetails?.comments?.length &&
-                          index === postDetails.comments.length - 1;
-                        return (
-                          <li
-                            key={index}
-                            className={`flex gap-3 ${!isLastComment ? 'border-b-[1px] pb-3' : ''}`}
-                          >
-                            <div className="flex h-[50px] w-[50px] shrink-0 items-center justify-center rounded-full bg-primary text-sm font-medium text-white">
-                              {comment.user ? getInitials(comment.user) : ''}
-                            </div>
-                            <div className="flex flex-col">
-                              <span className="text-xs font-medium text-black">{comment.user}</span>
-                              <span className="text-xs font-thin text-black">
-                                {comment.comment}
-                              </span>
-                            </div>
-                          </li>
-                        );
-                      })
-                    : null}
-                </ul>
+                {renderAddCommentForm()}
+                {renderComments()}
               </div>
             </div>
           </div>
