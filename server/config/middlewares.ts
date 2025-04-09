a89d470a30b1316acb0a7b08dc53eb80fdc67b59
Athul Rajeev <athul@evolvier.com>
2025-04-09 12:53:44 +0000

    upload size fix

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -30,7 +30,17 @@ export default [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // Form data limit
+      jsonLimit: '256mb', // JSON data limit
+      textLimit: '256mb', // Text data limit
+      formidable: {
+        maxFileSize: 10 * 1024 * 1024, // 10MB in bytes, adjust as needed
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
